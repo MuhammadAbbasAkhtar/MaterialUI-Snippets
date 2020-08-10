0a8656b8bed06edf05ebe371d133cb7e0dc9e23e
MuhammadAbbasAkhtar <44559259+MuhammadAbbasAkhtar@users.noreply.github.com>
2020-08-10 07:10:22 +0500

    added Letter Spacing changing Slider its basically a ranged slider having values -20 to 20, but only has 1 thumb.
    use this when you need either +ve or -ve values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 import FontSizeSelector from "./FontSizeSelector";
+import LetterSpacingChanger from "./LetterSpacingChanger";
+import Grid from "@material-ui/core/Grid";
 export default class App extends Component {
   render() {
     return (
       <div>
-        <FontSizeSelector />
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <FontSizeSelector />
+          </Grid>
+          <Grid item xs={12}>
+            <LetterSpacingChanger />
+          </Grid>
+        </Grid>
       </div>
     );
   }
---
+++ src/LetterSpacingChanger.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Slider from "@material-ui/core/Slider";
+
+let displayValue = 0;
+
+const useStyles = makeStyles({
+  root: {
+    width: 200
+  }
+});
+
+export default function LetterSpacingChanger() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState([0, 0]);
+
+  const handleChange = (e, newValue) => {
+    setValue(newValue);
+    const elem = e.currentTarget;
+    const spans = elem.querySelectorAll("[aria-label='letter-spacing']");
+
+    if (newValue[0] < 0) {
+      spans[0].style.display = "block";
+      displayValue = newValue[0];
+    } else {
+      spans[0].style.display = "none";
+    }
+
+    if (newValue[1] >= 1) {
+      spans[1].style.display = "block";
+      displayValue = newValue[1];
+    } else {
+      spans[1].style.display = "none";
+    }
+  };
+
+  return (
+    <div className={classes.root}>
+      <Typography id="letter-spacing" gutterBottom>
+        {displayValue}
+      </Typography>
+
+      <Slider
+        min={-21}
+        max={21}
+        value={value}
+        onChange={handleChange}
+        aria-label="letter-spacing"
+      />
+    </div>
+  );
+}
